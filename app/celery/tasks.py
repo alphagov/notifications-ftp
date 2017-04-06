dad96c59acb85587ffdebe20c10aaeb72068b890
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2017-04-06 16:58:23 +0100

    Ensure files removed in event of an error

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -18,9 +18,11 @@ def test():
 @notify_celery.task(name="send-files-to-dvla")
 @statsd(namespace="tasks")
 def send_files_to_dvla(jobs_ids):
-    ensure_local_file_directory()
-    for job_id in jobs_ids:
-        get_file_from_s3(current_app.config['DVLA_UPLOAD_BUCKET_NAME'], job_id)
-    dvla_file = concat_files()
-    ftp_client.send_file("{}/{}".format(current_app.config['LOCAL_FILE_STORAGE_PATH'], dvla_file))
-    remove_local_file_directory()
+    try:
+        ensure_local_file_directory()
+        for job_id in jobs_ids:
+            get_file_from_s3(current_app.config['DVLA_UPLOAD_BUCKET_NAME'], job_id)
+        dvla_file = concat_files()
+        ftp_client.send_file("{}/{}".format(current_app.config['LOCAL_FILE_STORAGE_PATH'], dvla_file))
+    finally:
+        remove_local_file_directory()
