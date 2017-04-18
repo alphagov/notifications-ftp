fffbeb0dd4685fcc05500012890170a05f8d2ae2
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2017-04-18 14:24:56 +0100

    tests using UUID not ints to prove the conversion to job_id from filename,

--- tests/app/celery/test_tasks.py
+++ tests/app/celery/test_tasks.py
@@ -1,5 +1,6 @@
 from app.celery.tasks import send_files_to_dvla
 import app
+from app.sftp.ftp_client import FtpException
 from unittest.mock import call
 from freezegun import freeze_time
 import pytest
@@ -169,10 +170,10 @@ def test_should_update_failed_tasks_with_unsuccesfully_processed_files(client, m
 def test_should_update_failed_tasks_with_unsuccesfully_processed_files_and_failed_s3_downloads(client, mocker):
     with freeze_time('2016-01-01T17:00:00'):
         def side_effect():
-            return "DVLA-FILE", ["2"], ["3"]
+            return "DVLA-FILE", ["3872ce4a-8817-44b9-bca6-972ac6706b59"], ["c278d13c-40cf-4091-ac88-3ef6eaeae4e8"]
 
         def failed_to_download(*args, **kwargs):
-            if args[1] == '1':
+            if args[1] == '0d0a0398-4c68-4c8d-9790-5d9632ecb1da':
                 return False
             return True
 
@@ -183,10 +184,40 @@ def test_should_update_failed_tasks_with_unsuccesfully_processed_files_and_faile
         mocker.patch('app.celery.tasks.ftp_client.send_file')
         mocker.patch('app.notify_celery.send_task')
 
-        send_files_to_dvla(["1", "2", "3"])
+        send_files_to_dvla([
+            "0d0a0398-4c68-4c8d-9790-5d9632ecb1da",
+            "3872ce4a-8817-44b9-bca6-972ac6706b59",
+            "c278d13c-40cf-4091-ac88-3ef6eaeae4e8"])
 
         assert app.notify_celery.send_task.call_args_list == [
-            call(name="update-letter-job-to-sent", args=("2",), queue="notify"),
-            call(name="update-letter-job-to-error", args=("1",), queue="notify"),
-            call(name="update-letter-job-to-error", args=("3",), queue="notify")
+            call(name="update-letter-job-to-sent", args=("3872ce4a-8817-44b9-bca6-972ac6706b59",), queue="notify"),
+            call(name="update-letter-job-to-error", args=("0d0a0398-4c68-4c8d-9790-5d9632ecb1da",), queue="notify"),
+            call(name="update-letter-job-to-error", args=("c278d13c-40cf-4091-ac88-3ef6eaeae4e8",), queue="notify")
+        ]
+
+
+def test_should_update_all_tasks_as_failed_if_ftp_fails(client, mocker):
+    with freeze_time('2016-01-01T17:00:00'):
+        def side_effect():
+            return "DVLA-FILE", [
+                "0d0a0398-4c68-4c8d-9790-5d9632ecb1da",
+                "3872ce4a-8817-44b9-bca6-972ac6706b59",
+                "c278d13c-40cf-4091-ac88-3ef6eaeae4e8"], []
+
+        mocker.patch('app.celery.tasks.ensure_local_file_directory')
+        mocker.patch('app.celery.tasks.get_file_from_s3')
+        mocker.patch('app.celery.tasks.concat_files', side_effect=side_effect)
+        mocker.patch('app.celery.tasks.remove_local_file_directory')
+        mocker.patch('app.celery.tasks.ftp_client.send_file', side_effect=FtpException("Oops"))
+        mocker.patch('app.notify_celery.send_task')
+
+        send_files_to_dvla([
+            "0d0a0398-4c68-4c8d-9790-5d9632ecb1da",
+            "3872ce4a-8817-44b9-bca6-972ac6706b59",
+            "c278d13c-40cf-4091-ac88-3ef6eaeae4e8"])
+
+        assert app.notify_celery.send_task.call_args_list == [
+            call(name="update-letter-job-to-error", args=("0d0a0398-4c68-4c8d-9790-5d9632ecb1da",), queue="notify"),
+            call(name="update-letter-job-to-error", args=("3872ce4a-8817-44b9-bca6-972ac6706b59",), queue="notify"),
+            call(name="update-letter-job-to-error", args=("c278d13c-40cf-4091-ac88-3ef6eaeae4e8",), queue="notify")
         ]
