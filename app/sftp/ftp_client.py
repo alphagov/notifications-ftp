3925f180851ef566617a5a07d905c04cab80781c
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2017-09-29 13:38:21 +0100

    add long overdue tests

--- app/sftp/ftp_client.py
+++ app/sftp/ftp_client.py
@@ -19,36 +19,40 @@ class FtpClient():
         self.statsd_client = statsd_client
 
     def send_file(self, local_file):
-        filename_without_path = os.path.split(local_file)[1]
         try:
             cnopts = pysftp.CnOpts()
             cnopts.hostkeys = None
             current_app.logger.info("opening connection to {}".format(self.host))
             with pysftp.Connection(self.host, username=self.username, password=self.password, cnopts=cnopts) as sftp:
-                sftp.chdir('notify')
-                current_app.logger.info("uploading {}".format(local_file))
-
-                start_time = monotonic()
-
-                remote_filename = get_dvla_file_name()
-
-                if sftp.exists('{}/{}'.format(sftp.pwd, remote_filename)):
-                    # increment the time in the filename by one minute - if there's ALSO a file with that name, then
-                    # lets just give up as something's definitely gone weird.
-                    remote_filename = get_new_dvla_filename(remote_filename)
-                    current_app.logger.warning('{} already exists on DVLA ftp, renaming to {}'.format(
-                        filename_without_path,
-                        remote_filename
-                    ))
-
-                sftp.put(local_file, remotepath='{}/{}'.format(sftp.pwd, remote_filename))
-
-                self.statsd_client.timing("ftp-client.upload-time", monotonic() - start_time)
-
-                if remote_filename in sftp.listdir():
-                    current_app.logger.info("Local file {} uploaded to DVLA as {}".format(local_file, remote_filename))
-                else:
-                    raise FtpException("File {} not uploaded".format(local_file))
+                upload_file(sftp, local_file, self.statsd_client)
         except Exception as e:
             current_app.logger.exception(e)
             raise FtpException("Failed to sFTP file")
+
+
+def upload_file(sftp, local_file, statsd_client):
+    filename_without_path = os.path.split(local_file)[1]
+    sftp.chdir('notify')
+    current_app.logger.info("uploading {}".format(local_file))
+
+    start_time = monotonic()
+
+    remote_filename = get_dvla_file_name()
+
+    if sftp.exists('{}/{}'.format(sftp.pwd, remote_filename)):
+        # increment the time in the filename by one minute - if there's ALSO a file with that name, then
+        # lets just give up as something's definitely gone weird.
+        remote_filename = get_new_dvla_filename(remote_filename)
+        current_app.logger.warning('{} already exists on DVLA ftp, renaming to {}'.format(
+            filename_without_path,
+            remote_filename
+        ))
+
+    sftp.put(local_file, remotepath='{}/{}'.format(sftp.pwd, remote_filename))
+
+    statsd_client.timing("ftp-client.upload-time", monotonic() - start_time)
+
+    if remote_filename in sftp.listdir():
+        current_app.logger.info("Local file {} uploaded to DVLA as {}".format(local_file, remote_filename))
+    else:
+        raise FtpException("File {} not uploaded".format(local_file))
---
+++ tests/app/sftp/test_ftp_client.py
@@ -0,0 +1,50 @@
+from unittest.mock import Mock
+
+import pytest
+from freezegun import freeze_time
+
+from app.sftp.ftp_client import upload_file, FtpException
+
+
+@freeze_time('2016-01-01T17:00:00')
+def test_send_file_generates_remote_filename():
+    local_file = '/tmp/something/foo.txt'
+    remote_filename = 'Notify-201601011700-rq.txt'
+    sftp = Mock(
+        pwd='~/notify',
+        exists=Mock(return_value=False),
+        listdir=Mock(return_value=[remote_filename])
+    )
+
+    upload_file(sftp, local_file, Mock())
+
+    sftp.chdir.assert_called_once_with('notify')
+    sftp.put.assert_called_once_with(local_file, remotepath='~/notify/' + remote_filename)
+
+
+@freeze_time('2016-01-01T17:00:00')
+def test_send_file_increments_filename_if_exists():
+    local_file = '/tmp/something/foo.txt'
+    remote_filename = 'Notify-201601011701-rq.txt'
+    sftp = Mock(
+        pwd='~/notify',
+        exists=Mock(return_value=True),
+        listdir=Mock(return_value=[remote_filename])
+    )
+
+    upload_file(sftp, local_file, Mock())
+
+    sftp.put.assert_called_once_with(local_file, remotepath='~/notify/' + remote_filename)
+
+
+@freeze_time('2016-01-01T17:00:00')
+def test_send_file_errors_if_file_wasnt_put_on():
+    local_file = '/tmp/something/foo.txt'
+    sftp = Mock(
+        pwd='~/notify',
+        exists=Mock(return_value=False),
+        listdir=Mock(return_value=[])
+    )
+
+    with pytest.raises(FtpException):
+        upload_file(sftp, local_file, Mock())
