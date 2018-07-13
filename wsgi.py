2a480b3057d0c66c00d82e611173b9e8e59a76a1
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2018-07-13 11:25:10 +0100

    add credstash back in for wsgi app

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+import os
+
 from flask import Flask
+from credstash import getAllSecrets
+
 from app import create_app
+
+os.environ['AWS_REGION'] = 'eu-west-1'
+os.environ.update(getAllSecrets(region="eu-west-1"))
 
 application = Flask('application')
 
