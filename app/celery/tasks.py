8385108a1c83051af4222e69f89af39f81bc52ab
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2018-11-27 13:46:23 +0000

    Now that we create pdfs from our templates we do not need many of the methods in this application. This just removes all that unused code.

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -6,12 +6,7 @@ from notifications_utils.s3 import s3upload as utils_s3upload
 
 from app import notify_celery, ftp_client
 from app.files.file_utils import (
-    get_job_from_s3,
-    get_api_from_s3,
     get_zip_of_letter_pdfs_from_s3,
-    concat_files,
-    LocalDir,
-    get_notification_references,
     get_notification_references_from_s3_filenames,
     get_dvla_file_name,
 )
@@ -19,59 +14,6 @@ from app.statsd_decorators import statsd
 from app.sftp.ftp_client import FtpException
 
 NOTIFY_QUEUE = 'notify-internal-tasks'
-
-
-@notify_celery.task(name="send-jobs-to-dvla")
-@statsd(namespace="tasks")
-def send_jobs_to_dvla(job_ids):
-    try:
-        with LocalDir('job') as job_folder:
-            job_filenames = [get_job_from_s3(job_id) for job_id in job_ids]
-            dvla_file = concat_files(job_filenames)
-            current_app.logger.info('Sending {} to dvla'.format(job_filenames))
-
-            ftp_client.send_file(
-                local_file=str(job_folder / dvla_file)
-            )
-    except Exception:
-        current_app.logger.exception('FTP app failed to send jobs')
-        task_name = 'update-letter-job-to-error'
-    else:
-        task_name = 'update-letter-job-to-sent'
-    finally:
-        for job_id in job_ids:
-            notify_celery.send_task(
-                name=task_name, args=(job_id,), queue=NOTIFY_QUEUE
-            )
-
-
-@notify_celery.task(name="send-api-notifications-to-dvla")
-@statsd(namespace="tasks")
-def send_api_notifications_to_dvla(filename):
-    # get pipe file from S3
-    with LocalDir('api') as api_folder:
-        get_api_from_s3(filename)
-
-        notification_references = get_notification_references(filename)
-        current_app.logger.info(
-            'Sending {} notifications from {} to dvla'.format(
-                len(notification_references),
-                filename
-            )
-        )
-
-        try:
-            ftp_client.send_file(
-                local_file=str(api_folder / filename)
-            )
-        except FtpException:
-            # if there's an s3 error we won't know what notifications to update, so only worry about FTP issues
-            current_app.logger.exception('FTP app failed to send api messages')
-            task_name = "update-letter-notifications-to-error"
-        else:
-            task_name = "update-letter-notifications-to-sent"
-
-        update_notifications(task_name, notification_references)
 
 
 @notify_celery.task(name="zip-and-send-letter-pdfs")
