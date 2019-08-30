7b76ff7f7f26eaca2487551ef81c03d8756f71a2
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2019-08-30 15:30:11 +0100

    Revert "Drop credstash as a dependency"
    This reverts commit 9cd968b83ccc7c6a3b3f16ddf634c5edd2c9dc64.

--- aws_run_celery.py
+++ aws_run_celery.py
@@ -2,9 +2,11 @@
 from flask import Flask
 
 from app import notify_celery, create_app  # noqa: notify_celery required to get celery running
+from credstash import getAllSecrets
 import os
 
 os.environ['AWS_REGION'] = 'eu-west-1'
+os.environ.update(getAllSecrets(region="eu-west-1"))
 
 application = Flask('delivery')
 create_app(application)
