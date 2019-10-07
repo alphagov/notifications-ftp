ab3a38b71a76f37b306f17b70a4b4c9bb0c781f9
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2019-10-07 14:22:20 +0100

    Removed getAllSecrets from credstash since it's not used anymore. Fix Makefile to include the CF_API url

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ from credstash import getAllSecrets
 from app import create_app
 
 os.environ['AWS_REGION'] = 'eu-west-1'
-os.environ.update(getAllSecrets(region="eu-west-1"))
+# os.environ.update(getAllSecrets(region="eu-west-1"))
 
 application = Flask('application')
 
