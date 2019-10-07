f0ea1e5868f4faeaa7fca2b2a3406b3930e43bb9
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2019-10-07 14:24:52 +0100

    Remove comment

--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,6 @@ from credstash import getAllSecrets
 from app import create_app
 
 os.environ['AWS_REGION'] = 'eu-west-1'
-# os.environ.update(getAllSecrets(region="eu-west-1"))
-
 application = Flask('application')
 
 create_app(application)
