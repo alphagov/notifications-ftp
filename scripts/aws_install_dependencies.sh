e2b181e9e19f1f39ee2ccad60907ddb1447c4d99
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2019-12-13 15:05:02 +0000

    Remove old files related to AWS deployment
    We no longer deploy this onto AWS
    
    Note, we still keep the awslogs file as we still ship logs to cloudwatch
    from the PaaS

--- scripts/aws_install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-echo "Install dependencies"
-
-cd /home/notify-app/notifications-ftp;
-# had problems with crypto (required by pysftp) installing cffi, so trying to install it ourselves beforehand
-pip3 install --find-links=wheelhouse -r /home/notify-app/notifications-ftp/requirements.txt
