1601af6c0b7325d2f66df99868e908ef0d847168
sakisv <me@sakisv.net>
2021-02-04 19:32:35 +0200

    Increase worker_max_tasks_per_child to 20
    2GB are more than enough to be able to handle memory leaks from 20 zip
    files. The memory usage on production during the sending period doesn't
    even go to 10%

--- app/config.py
+++ app/config.py
@@ -38,7 +38,7 @@ class Config(object):
         # restart workers after each task is executed - this will help prevent any memory leaks (not that we should be
         # encouraging sloppy memory management). Since we only run a handful of tasks per day, and none are time
         # sensitive, the extra couple of seconds overhead isn't seen to be a huge issue.
-        'worker_max_tasks_per_child': 1
+        'worker_max_tasks_per_child': 20
     }
 
     STATSD_ENABLED = False
